737531606dc425a47489c60a23a4e896523bb0f4
endo <endo@Ln013>
2014-08-01 17:57:56 +0900

    コメント削除

--- skillcheck/src/jp/co/brainnet/skillcheck/b/Question6.java
+++ skillcheck/src/jp/co/brainnet/skillcheck/b/Question6.java
@@ -13,12 +13,10 @@ public class Question6 extends HttpServlet {
       throws ServletException, IOException {
 
     // HTTPヘッダに出力形式を指定
-//     response.setContentType("text/html");
     response.setContentType("text/html;charset=UTF-8");
 
     // 出力ストリームの取得
     PrintWriter out = response.getWriter();
-    // ServletOutputStream out = response.getOutputStream();
 
     // HTTP本文へHTML文字列の出力
     out.println("<HTML><HEAD><TITLE>練習問題６</TITLE></HEAD>");
