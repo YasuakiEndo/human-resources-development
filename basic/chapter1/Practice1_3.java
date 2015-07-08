9d30a321b68a36d6494208588bb675e7ffbd5966
YasuakiEndo <y.endo.bn@gmail.com>
2015-07-08 15:24:15 +0900

    Create Practice1_3.java

---
+++ basic/chapter1/Practice1_3.java
@@ -0,0 +1,9 @@
+public class Practice1_3 {
+  public static void main(String... args) {
+    boolean result = true;          // ①
+    char favoriteCharacter = '駆';  // ②
+    double pi = 3.14;               // ③
+    long number = 314159265853979L; // ④
+    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";  // ⑤
+  }
+}
